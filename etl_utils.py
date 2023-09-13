2b4280488b8b91b0cabd884f49565000b34d9231
Sander Idelson <sander.idelson@gmail.com>
2023-09-12 21:16:35 -0700

    lots of updates to scripts - its working

--- etl_utils.py
+++ etl_utils.py
@@ -18,37 +18,44 @@ def windows(data, window_size, step):
     return pd.concat(map(g, z), keys=map(f, z))
 
 
-def get_feature_embedding_for_window(df, stock):
+def get_feature_embedding_for_window(df, stock, split=64):
     ts_name = f"{stock.strip('.csv')}_{str(df.Date.min())}_{str(df.Date.max())}"
     scaler=MinMaxScaler()
     df[['Open', 'Close']] = scaler.fit_transform(df[['Open', 'Close']])
+    if (df['Open'].values == 0).all():
+        df['Open'] = df['Close']
+
     prices = df[['Open', 'Close']].values.tolist()
     flat_values = [item for sublist in prices for item in sublist]
     df = df.rename(columns={"Date":"time"}) 
     ts_df = pd.DataFrame({'time':df.time.repeat(2), 
                           'price':flat_values})
     ts_df.drop_duplicates(keep='first', inplace=True)  
+    pre_flat_values = flat_values[:split*2]
+    post_flat_values = flat_values[split*2:]
 
     # Use Kats to extract features for the time window
     try:
         if not (len(np.unique(ts_df.price.tolist())) == 1 \
             or len(np.unique(ts_df.price.tolist())) == 0):
-            timeseries = TimeSeriesData(ts_df)
+            timeseries = TimeSeriesData(ts_df[:split*2])
             features = TsFeatures().transform(timeseries)
             feature_list = [float(v) if not pd.isnull(v) else float(0) for _, v in features.items()]
+            feature_list = [v / (split*2.0) if v > 1.0 else v for v in feature_list]
             if Decimal('Infinity') in feature_list or Decimal('-Infinity') in feature_list:
-                return None
-            return (ts_name, feature_list)
+                return None, None
+            feature_list = pre_flat_values + feature_list
+            return (ts_name, feature_list), (ts_name, post_flat_values)
     except np.linalg.LinAlgError as e:
         print(f"Can't process {ts_name}:{e}")
-    return None
+    return None, None
 
 
 def get_simple_pair_for_window(df, stock, split=64):
     ts_name = f"{stock.strip('.csv')}_{str(df.Date.min())}_{str(df.Date.max())}"
     prices = df[['Open', 'Close']].values.tolist()
-    flat_values = [item for sublist in prices[:split] for item in sublist]
-    post_flat_values = [item for sublist in prices[split:] for item in sublist]
+    flat_values = [item for sublist in prices[:split*2] for item in sublist]
+    post_flat_values = [item for sublist in prices[split*2:] for item in sublist]
     return (ts_name, flat_values), (ts_name, post_flat_values)
 
 
@@ -99,7 +106,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--window_size', type=int, default=64)
     parser.add_argument('--post_window_size', type=int, default=16)
-    parser.add_argument('--step', type=int, default=10)
+    parser.add_argument('--step', type=int, default=32)
     parser.add_argument('--folder_name', type=str, default='./data/stocks')
     parser.add_argument('--num_stocks', type=int, default=1)
 
@@ -112,7 +119,8 @@ def main():
 
     for stock in stocks[:args.num_stocks]:
         feature_pairs, feature_post = create_sliding_window_feature(stock,
-                                                                    get_simple_pair_for_window,
+                                                                    get_feature_embedding_for_window,
+                                                                    # get_simple_pair_for_window,
                                                                     window_size=args.window_size,
                                                                     post_window_size=args.post_window_size,
                                                                     step=args.step)
